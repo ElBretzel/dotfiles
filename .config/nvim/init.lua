96064ee4d4313cdfc22acd660d2c7f473713027f
luca.descrambes <luca.descrambes@epita.fr>
2024-06-23 09:40:49 +0000

    feat: support for distrobox

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.opt.mouse = ""
-vim.o.ttymouse = ""
-vim.o.cc = 80
+vim.o.cc = "80"
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
@@ -11,7 +10,6 @@ vim.o.list = true
 vim.o.listchars = "eol:⏎,tab:- ,space:."
 vim.o.cindent = true
 vim.o.textwidth = 120
-vim.o.belloff = true
 vim.o.autoindent = true
 vim.o.wrapmargin = 20
 vim.o.linebreak = true
